bd3f43fc1d870a8d518beb6e366a793f597ba2b1
elpiedra83 <luis.piedrahita@outlook.com>
2021-03-09 16:55:14 -0300

    first commit

---
+++ src/helpers.ts
@@ -0,0 +1,34 @@
+import { Brick } from "./sprites/Brick";
+import {
+  BRICK_IMAGES,
+  LEVEL,
+  STAGE_COLS,
+  STAGE_PADDING,
+  BRICK_WIDTH,
+  BRICK_HEIGHT,
+  BRICK_PADDING,
+  BRICK_ENERGY,
+} from "./setup";
+
+export function createBricks(): Brick[] {
+  return LEVEL.reduce((ack, element, i) => {
+    const row = Math.floor((i + 1) / STAGE_COLS); //gives the current row of the specific brick
+    const col = i % STAGE_COLS; //correct column for the specific brick
+
+    const x = STAGE_PADDING + col * (BRICK_WIDTH + BRICK_PADDING); //STAGE_PADDING is the space between walls and canvas
+    const y = STAGE_PADDING + row * (BRICK_HEIGHT + BRICK_PADDING);
+
+    if (element === 0) return ack;
+
+    return [
+      ...ack,
+      new Brick(
+        BRICK_WIDTH,
+        BRICK_HEIGHT,
+        { x, y },
+        BRICK_ENERGY[element],
+        BRICK_IMAGES[element]
+      ),
+    ];
+  }, [] as Brick[]);
+}
